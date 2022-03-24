081d7b1fc8b35dcf7a74a519addcdf4335003c21
Indrajit <indrajit.parmar@clariontechnologies.co.in>
2022-03-25 00:04:37 +0530

    footer year changes

--- footer.php
+++ footer.php
@@ -1,9 +1,12 @@
    <!-- Footer -->
-    <div id="footer">
-        <div id="top" class="noprint"><p><span class="noscreen">Back on top</span> <a href="#header" title="Back on top ^">^<span></span></a></p></div>
-        <hr class="noscreen" />
-        
-        <p class="style1" id="createdby">created by Our Team<!-- DON´T REMOVE, PLEASE! -->
-        </p>
-        <p id="copyright">&copy; 2013 xyzjob.com</p>
-</div> <!-- /footer -->
+   <div id="footer">
+       <div id="top" class="noprint">
+           <p><span class="noscreen">Back on top</span> <a href="#header" title="Back on top ^">^<span></span></a></p>
+       </div>
+       <hr class="noscreen" />
+
+       <p class="style1" id="createdby">created by Our Team
+           <!-- DON´T REMOVE, PLEASE! -->
+       </p>
+       <p id="copyright">&copy; <?php echo date('Y'); ?> xyzjob.com</p>
+   </div> <!-- /footer -->
